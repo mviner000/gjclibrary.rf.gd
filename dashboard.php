182831f00b126f60b8e6c7581e286b58b22d9146
mviner000 <121312294+mviner000@users.noreply.github.com>
2024-05-05 22:25:36 +0800

    updated with users list

--- dashboard.php
+++ dashboard.php
@@ -45,19 +45,33 @@ if (isset($_GET['logout']) && $_GET['logout'] === 'true') {
 ?>
 
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>GJCLibrary - Dashboard</title>
 </head>
-<body>
-<div class="container mx-auto px-4 py-8">
-    <h1 class="text-center mt-8 font-bold text-4xl mb-4"><a href="https://gjclibrary.com">GJCLibrary</a></h1>
-    <h1 class="text-2xl font-medium text-center mb-8">Welcome, <?php echo $_SESSION['user']['username']; ?>!</h1>
-    <div class="flex justify-center">
-        <a href="dashboard.php?logout=true" class="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded">Logout</a>
+<body class="h-full flex justify-center items-center">
+  <div class="container mx-auto p-4">
+    <nav class="flex justify-center mb-4">
+      <ul class="flex justify-center">
+        <li class="mr-6">
+          <a href="#" class="opacity-50 text-blue-500 hover:text-blue-800 font-extrabold">Dashboard</a>
+        </li>
+        <li class="mr-6">
+          <a href="user-list.php" class="text-emerald-500 hover:text-blue-800 font-bold">Users List</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="flex justify-center mb-8">
+      <img src="images/session_destroy.jpg" alt="destroy task" class="border-2 border-cyan-700 shadow-md">
     </div>
-</div>
+    <h1 class="text-4xl font-bold text-center mb-4"><a href="https://gjclibrary.com">GJCLibrary</a></h1>
+    <h2 class="text-2xl font-medium text-center mb-8">Welcome, <?= $_SESSION['user']['username'];?>!</h2>
+    <div class="flex justify-center">
+      <a href="dashboard.php?logout=true" class="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded">Logout</a>
+    </div>
+  </div>
 </body>
 </html>
---
+++ user-list.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>GJCLibrary</title>
+</head>
+<body class="h-full flex justify-center items-center">
+  <div class="container mx-auto p-4">
+    <nav class="flex justify-center mb-4">
+      <ul class="flex justify-center">
+        <li class="mr-6">
+          <a href="dashboard.php" class="text-emerald-500 hover:text-blue-800 font-bold">Dashboard</a>
+        </li>
+        <li class="mr-6">
+          <a href="user-list.php" class="opacity-50 text-blue-500 hover:text-blue-800 font-extrabold">Users List</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="flex justify-center mb-8">
+      <img src="images/list_users.jpg" alt="destroy task" class="border-2 border-cyan-700 shadow-md">
+    </div>
+    <h1 class="text-4xl font-bold text-center mb-4"><a href="https://gjclibrary.com">GJCLibrary</a></h1>
+    <h2 class="text-2xl font-medium text-center mb-8">User list will be in here</h2>
+    <div class="alert alert-red mb-8 lg:w-1/2 mx-auto">
+  <div class="alert-header bg-red-500 text-white font-bold rounded-t px-4 py-2">
+    TO-DO! Priority - High
+  </div>
+  <div class="flex alert-body border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+    <p>Sir, Update na lang po namin, Di pa finish.</p>
+  </div>
+</div>
+  </div>
+</body>
+</html>
